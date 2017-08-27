d53abaf7edaf9ec20b6644fe7c446541923e3e1e
Lukas Leppich <lukas.leppich@gmail.com>
2017-08-27 08:52:54 +0200

    Add noconfirm

--- termite/install.sh
+++ termite/install.sh
@@ -2,7 +2,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-sudo pacman -S termite
+sudo pacman -S termite --noconfirm
 
 mkdir -p ~/.config/termite
 cp config ~/.config/termite/config
