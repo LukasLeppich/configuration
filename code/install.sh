f93864ca83cc0a68758713d3f00c59275dab8aa0
Lukas Leppich <lukas.leppich@gmail.com>
2017-08-27 08:52:08 +0200

    Fix vs code install

--- code/install.sh
+++ code/install.sh
@@ -3,7 +3,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 
-# yaourt -S visual-studio-code
-
+yaourt -S visual-studio-code --noconfirm
+code --list-extensions
 rsync -avP *.json ~/.config/Code/User/
 rsync -avP snippets ~/.config/Code/User/
